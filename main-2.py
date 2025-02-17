46517074f10f453f7f4c07406e97b39678df4fcf
Enderson Menezes (Mr. Enderson) <endersonster@gmail.com>
2025-02-17 22:31:52 +0000

    Adiciona validação de dias e lógica para atividades específicas

--- main-2.py
+++ main-2.py
@@ -1,9 +1,21 @@
 # Meu primeiro comentário
 # Apresentando meu primeiro algoritmo
 dia = input("Que dia é hoje?!")
-if dia = "quinta":
-  
-
+valid_days = ["segunda", "terça", "quarta", "quinta", "sexta", "sábado", "domingo"]
+if dia not in valid_days:
+    print("Dia inválido")
+    exit()
+if dia == "quinta":
+    print("Hoje é dia de feira, não preciso me trocar!")
+elif dia == "terça":
+    print("Hoje é dia de aula de música!")
 print("Pegar o elevador")
 print("Ir na venda")
 print("Comprar chiclete")
+chiclete = 0
+while chiclete < 10:
+    print("Mascar chiclete")
+    chiclete += 1
+    if chiclete == 9:
+        print("Está ficando sem gosto!")
+print("Jogar chiclete fora")
