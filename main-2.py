c3eaacd9fbb10bf2dfa653479f6b9422697e9378
Enderson Menezes (Mr. Enderson) <endersonster@gmail.com>
2025-02-17 19:22:54 -0300

    Create main-2.py

---
+++ main-2.py
@@ -0,0 +1,9 @@
+# Meu primeiro comentário
+# Apresentando meu primeiro algoritmo
+dia = input("Que dia é hoje?!")
+if dia = "quinta":
+  
+
+print("Pegar o elevador")
+print("Ir na venda")
+print("Comprar chiclete")
